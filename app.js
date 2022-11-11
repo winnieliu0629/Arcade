28e6e92497b39da8052a70888f2080979dccf970
Winnie Liu <pewinonlyy@gmail.com>
2022-11-10 19:55:45 -0800

    fixed naming issue

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ const gameState = {
         }
         return false
     },
+    checkNull: function() {
+        for(let i = 0; i < this.board.length; i++){
+            for(let j = 0; j < this.board.length; j++){
+                if(this.board[i][j] == null){
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
     move: function(char, numRows, numColumns) {
         if (this.board[numRows][numColumns] === null) {
           this.board[numRows][numColumns] = char;
@@ -71,9 +81,8 @@ function includesThree(arr) {
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const playerName = document.querySelector('#playerName');
-const n1 = document.querySelector('#n1');
-const n2 = document.querySelector('#n2');
-const nameInput = document.querySelector('#nameInput');
+const p1Label = document.querySelector('#p1Label');
+const p2Label = document.querySelector('#p2Label');
 const p1Input = document.querySelector('#p1Input');
 const p2Input = document.querySelector('#p2Input');
 const submit = document.querySelector('#submit');
@@ -82,8 +91,6 @@ const p1Name = document.querySelector('#p1Name');
 const p2Name = document.querySelector('#p2Name');
 const hint = document.querySelector('#hint');
 const board = document.querySelector('#board');
-const newGame = document.querySelector('#newGame');
-const reset = document.querySelector('#reset');
 
 let p1ButtonClicked = false;
 let p2ButtonClicked = false;
@@ -92,7 +99,9 @@ p1Button.addEventListener('click', function() {
     p1Button.hidden = true;
     p2Button.hidden = true;
     playerName.hidden = false;
-    n1.hidden = false;
+    p1Label.hidden = false;
+    p1Label.textContent = 'Enter your name:';
+    p1Input.hidden = false;
     submit.hidden = false;
     p1ButtonClicked = true;
 })
@@ -101,37 +110,48 @@ p2Button.addEventListener('click', function() {
     p1Button.hidden = true;
     p2Button.hidden = true;
     playerName.hidden = false;
-    n2.hidden = false;
+    p1Label.hidden = false;
+    p1Label.textContent = 'Player1 name:';
+    p2Label.hidden = false;
+    p1Input.hidden = false;
+    p2Input.hidden = false;
     submit.hidden = false;
     p2ButtonClicked = true;
 })
 
 submit.addEventListener('click',function() {
     playerName.hidden = true;
+    p1Label.hidden = true;
+    p2Label.hidden = true;
+    p1Input.hidden = true;
+    p2Input.hidden = true;   
     submit.hidden = true;
     hint.hidden = false;
     board.hidden = false;
-    n1.hidden = true;
-    n2.hidden = true;
     scores.hidden = false;
-    if(p1ButtonClicked) {
-        if(nameInput.value) {
-            p1Name.textContent = `${nameInput.value}(O)`;
-        }
-    } else {
-        if(p1Input.value) {
-            p1Name.textContent = `${p1Input.value}(O)`;
-        } else {
-            p1Name.textContent = 'Player 1(O)';
-        }
-        if(p2Input.value) {
-            p2Name.textContent = `${p2Input.value}(X)`;
-        } else {
-            p2Name.textContent = 'Player 2(X)';
-        }
-    }
+    updateName();
     renderBoard();
 })
+
+function updateName() {
+    if(p1ButtonClicked) {
+        if(!p1Input.value) {
+            p1Input.value = 'You';
+        }
+        p2Input.value = 'Computer';
+        p1Name.textContent = `${p1Input.value}(O)`;
+        p2Name.textContent = `${p2Input.value}(X)`;
+    } else {
+        if(!p1Input.value) {
+            p1Input.value = 'Player 1';
+        }
+        if(!p2Input.value) {
+            p2Input.value = 'Player 2';
+        } 
+        p1Name.textContent = `${p1Input.value}(O)`;
+        p2Name.textContent = `${p2Input.value}(X)`;
+    }
+}
 
 function renderBoard() {
     board.innerHTML = '';
@@ -168,14 +188,44 @@ board.addEventListener('click', function(event) {
     renderBoard();
 })
 
+const p1WinEl = document.querySelector('#p1WinEl');
+const tieEl = document.querySelector('#tieEl');
+const p2WinEl = document.querySelector('#p2WinEl');
+let p1Win = 0;
+let tie = 0;
+let p2Win = 0;
+
 function checkWin() {
     if(gameState.checkLine() == true) {
-        hint.textContent = `${gameState.players[gameState.currentPlayer]} win!`
+        if(gameState.currentPlayer == 0) {
+            p1Win++;
+            hint.textContent = `${p1Input.value} win!`
+            gameState.clear();
+            changePlayer();
+        } else {
+            p2Win++;
+            hint.textContent = `${p2Input.value} win!`
+            gameState.clear();
+            changePlayer();
+        }
+    } else if(gameState.checkNull() == false) {
+        tie++;
+        hint.textContent = `Tie!`
+        gameState.clear();
+        changePlayer();
     } else {
         changePlayer();
         hint.textContent = `${gameState.players[gameState.currentPlayer]} turn`
     }
+    updateScores();
 }
+
+function updateScores() {
+    p1WinEl.textContent = p1Win;
+    tieEl.textContent = tie;
+    p2WinEl.textContent = p2Win;
+}
+
 function changePlayer() {
     gameState.currentPlayer = gameState.currentPlayer === 0 ? 1 : 0;
 }
@@ -184,20 +234,40 @@ function changePlayer() {
 
 // },1000)
 
+const newGame = document.querySelector('#newGame');
+const reset = document.querySelector('#reset');
+
 newGame.addEventListener('click', function() {
     p1Button.hidden = false;
     p2Button.hidden = false;
     playerName.hidden = true;
+    p1Label.hidden = true;
+    p2Label.hidden = true;
+    p1Input.hidden = true;
+    p2Input.hidden = true;
     submit.hidden = true;
-    n1.hidden = true;
-    n2.hidden = true;
     scores.hidden = true;
     hint.hidden = true;
     board.innerHTML = '';
-    gameState.clear;
+    resetGame();
+    p1Input.value = '';
+    p2Input.value = '';
+    p1Name.textContent = '';
+    p2Name.textContent = '';
+    p1ButtonClicked = false;
+    p2ButtonClicked = false;
 })
 
 reset.addEventListener('click', function() {
-    gameState.clear();
-    console.log(gameState.board);
+    resetGame();
+    renderBoard();
 })
+
+function resetGame() {
+    p1Win = 0;
+    tie = 0;
+    p2Win = 0;
+    updateScores();
+    gameState.clear();
+    hint.textContent = 'Place three in a row!';
+}
